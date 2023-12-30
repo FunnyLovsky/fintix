7d6d7f951d6d35301b00dcdb7159aa1cbccbdcab
Andrey Veselovsky <142045012+FunnyLovsky@users.noreply.github.com>
2023-12-30 15:22:31 +0300

    dev back: install depend, setting eslint, prettier, tsconfig

---
+++ backend/src/index.ts
@@ -0,0 +1,21 @@
+import express from 'express';
+import cors from 'cors';
+
+const app = express();
+const PORT = 5000;
+
+app.use(express.json());
+app.use(cors());
+
+const start = async () => {
+    try {
+        app.listen(PORT, () => {
+            console.log('Server start on port:', PORT);
+        });
+    } catch (error) {
+        console.log(error);
+    }
+console.log()
+};
+
+start();
